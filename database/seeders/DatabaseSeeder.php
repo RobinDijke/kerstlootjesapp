2e35b3365933f3d638c4e4d2ec59c6d811e79629
Fedde van Gils <feddevangils@gmail.com>
2022-11-22 13:33:23 +0100

    edit homepage + add new user in seerder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,6 +23,14 @@ class DatabaseSeeder extends Seeder
             'name' => 'admin'
         ]);
 
+        \App\Models\User::create(['name' => fake()->name(),
+            'email' => 'f.vangils@curio.nl',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+            'role_id' => 2
+        ]);
+
         \App\Models\User::factory(50)->create();
 
         \DB::table('groups')->insert([
